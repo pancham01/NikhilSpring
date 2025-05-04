b0d93a137fc040e2bdf809d21948c7fe051f1a4a
pancham01 <pancham9557@gmail.com>
2025-05-04 13:57:16 +0530

    update

--- src/main/java/com/explore/model/Employee.java
+++ src/main/java/com/explore/model/Employee.java
@@ -1,12 +1,20 @@
 package com.explore.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Employee {
 
 	private int id;
 
 	private String name, gender;
-	
+
 	private Address address;
+
+	private List<Integer> list;
+	private Set<Integer> set;
+	private Map<Integer, String> map;
 
 	public Employee() {
 		super();
@@ -14,29 +22,57 @@ public class Employee {
 	}
 
 	
-
-	public Employee(int id, String name, String gender, Address address) {
+	public Employee(int id, String name, String gender, Address address, List<Integer> list, Set<Integer> set,
+			Map<Integer, String> map) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.address = address;
+		this.list = list;
+		this.set = set;
+		this.map = map;
 	}
 
+
+	public List<Integer> getList() {
+		return list;
+	}
+
+
+	public void setList(List<Integer> list) {
+		this.list = list;
+	}
+
+
+	public Set<Integer> getSet() {
+		return set;
+	}
+
+
+	public void setSet(Set<Integer> set) {
+		this.set = set;
+	}
+
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
+	}
 
 
 	public Address getAddress() {
 		return address;
 	}
 
-
-
 	public void setAddress(Address address) {
 		System.out.println("Employee.setAddress()");
 		this.address = address;
 	}
-
-
 
 	public int getId() {
 		return id;
@@ -65,9 +101,11 @@ public class Employee {
 		this.gender = gender;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + ", list="
+				+ list + ", set=" + set + ", map=" + map + "]";
 	}
 
 }
